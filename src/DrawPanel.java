254a34ed710da070c170dc9ea21f1b8d52db94e8
Heseral <artemka-sapelkin@mail.ru>
2020-12-04 19:54:06 +0300

    мелкоправки для удобства; мат парсер

---
+++ src/DrawPanel.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
+import draw.IDrawer;
+import draw.SimpleEdgeDrawer;
+import math.Vector3;
+import screen.ScreenConverter;
+import third.Camera;
+import third.Scene;
+import models.Parallelepiped;
+
+/**
+ *
+ * @author Alexey
+ */
+public class DrawPanel extends JPanel
+        implements CameraController.RepaintListener {
+    private Scene scene;
+    private ScreenConverter sc;
+    private Camera cam;
+    private CameraController camController;
+    
+    public DrawPanel() {
+        super();
+        sc = new ScreenConverter(-1, 1, 2, 2, 1, 1);
+        cam = new Camera();
+        camController = new CameraController(cam, sc);
+        scene = new Scene(Color.WHITE.getRGB());
+        scene.showAxes();
+        
+        scene.getModelsList().add(new Parallelepiped(
+                new Vector3(-0.4f, -0.4f, -0.4f), 
+                new Vector3(0.4f, 0.4f, 0.4f)
+        ));
+        
+        camController.addRepaintListener(this);
+        addMouseListener(camController);
+        addMouseMotionListener(camController);
+        addMouseWheelListener(camController);
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        sc.setScreenSize(getWidth(), getHeight());
+        BufferedImage bi = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = (Graphics2D)bi.getGraphics();
+        IDrawer dr = new SimpleEdgeDrawer(sc, graphics);
+        scene.drawScene(dr, cam);
+        g.drawImage(bi, 0, 0, null);
+        graphics.dispose();
+    }
+
+    @Override
+    public void shouldRepaint() {
+        repaint();
+    }
+}
---
+++ symja-parser2016-02-06/symja-parser.bat
@@ -0,0 +1,2 @@
+REM run symja parser
+"C:\Program Files (x86)\Java\jre7\bin\java" -classpath symja-parser2016-02-06.jar;commons-math3-3.6.jar  org.matheclipse.parser.util.Console
